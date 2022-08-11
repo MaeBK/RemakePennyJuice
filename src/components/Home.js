5096e07d2b238572ba5c2c9d9c9a7d7737db05b4
CameronTwiggs <cameron.020617@gmail.com>
2022-08-11 10:35:58 -0400

    adds a loading to signify that text IS cominging in

--- src/components/Home.js
+++ src/components/Home.js
@@ -17,11 +17,12 @@ class Home extends React.Component{
             .then(data => this.setState({ data: data.slip.advice, isFetching: false }));
     }
 
+ 
     render() {
         return (
             <>
-            <h1>This is the home page</h1>
-            <h2>{this.state.data}</h2>
+                <h1>This is the home page</h1>
+                <p>{this.state.isFetching ? "Loading..." : this.state.data}</p>
             </>
 
             )
