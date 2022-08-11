f7dbf04bc09848c222738500090701bfc5bde2c3
CameronTwiggs <cameron.020617@gmail.com>
2022-08-11 10:26:55 -0400

    adds quote api to the main page

--- src/components/Aboutus.js
+++ src/components/Aboutus.js
@@ -4,8 +4,8 @@ import React from 'react';
 class Aboutus extends React.Component{
     render() {
         return (
+
             <>
-           
             <h1>This is the about us page</h1>
             </>
                 
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,10 +2,26 @@ import React from 'react';
 
 
 class Home extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+                isFetching: false,
+                data: []
+        }
+    }
+
+    componentDidMount() {
+        this.setState({ isFetching: true });
+        fetch("https://api.adviceslip.com/advice")
+            .then(response => response.json())
+            .then(data => this.setState({ data: data.slip.advice, isFetching: false }));
+    }
+
     render() {
         return (
             <>
             <h1>This is the home page</h1>
+            <h2>{this.state.data}</h2>
             </>
 
             )
